a3eb11deef8b50a8fa24c4c83b40ea46f7d0eb64
mathieugery <mathieu.gery@epitech.eu>
2021-02-18 21:11:24 +0100

    Search bar integration

---
+++ lib/Views/favorites.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Favorites extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Favorites',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
