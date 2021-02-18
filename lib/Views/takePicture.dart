36717a4d2aec465d2835bba45c5621ec9206d6cb
mathieugery <mathieu.gery@epitech.eu>
2021-02-18 11:52:51 +0100

    [+] Take picture button navbar and view

---
+++ lib/Views/takePicture.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TakePicture extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Take Picture',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
