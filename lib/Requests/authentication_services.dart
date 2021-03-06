5e6db110128d83524aa95e99c7eb1bb789ee73e2
romain <romain.floriani@eptiech.eu>
2021-03-06 20:55:55 +0100

    [+] User collection creation

--- lib/Requests/authentication_services.dart
+++ lib/Requests/authentication_services.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth;
@@ -17,10 +18,31 @@ class AuthenticationService {
     }
   }
 
-  Future<String?> signUp(String email, String password) async {
+  Future<String?> signUp(String name, String username, String email,
+      String password, String confirmPassword, String biography) async {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    QuerySnapshot usernameCheck =
+        await users.where('username', isEqualTo: username).get();
+    QuerySnapshot emailCheck =
+        await users.where('email', isEqualTo: email).get();
+
+    if (usernameCheck.docs.isEmpty) {
+      return ("User with this username already exists.");
+    }
+    if (emailCheck.docs.isEmpty) {
+      return ("User with this email already exists.");
+    }
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
+      final userID = FirebaseAuth.instance.currentUser!.uid;
+
+      await users.doc(userID).set({
+        'name': name,
+        'username': username,
+        'email': email,
+        'biography': biography
+      }).catchError((error) => print("Failed to add user $error"));
       return "Registered in";
     } on FirebaseAuthException catch (err) {
       return err.message;
